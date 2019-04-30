60df37e20f46864cd316ecf9eb25bae1bf710d78
lms <lms@skbkontur.ru>
2019-04-30 10:31:37 +0500

    fix references

--- TransportSystem.Web/ClientApp/src/App.js
+++ TransportSystem.Web/ClientApp/src/App.js
@@ -1,5 +1,5 @@
 ﻿import React from 'react';
-import { Route } from 'react-router';
+import { Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
