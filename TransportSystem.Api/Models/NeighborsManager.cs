c9d5b3994b299f22cff72a9066877e813881d33b
lms <lms@skbkontur.ru>
2019-04-30 10:22:15 +0500

    add NeighboursManager

--- TransportSystem.Api/Models/NeighborsManager.cs
+++ TransportSystem.Api/Models/NeighborsManager.cs
@@ -7,43 +7,55 @@ namespace TransportSystem.Api.Models
 {
     public class NeighborsManager
     {
-        private readonly SmoPassenger[] smoPassengers;
+        private readonly SmoPassengerInfo[] smoPassengers;
         private readonly int columns;
         private readonly int neighboursCount;
         private Dictionary<string, List<string>> idToNeighbours;
         private Random random;
 
-        public NeighborsManager(SmoPassenger[] smoPassengers, int columns, int neighboursCount)
+        public NeighborsManager(SmoPassengerInfo[] smoPassengers, int columns, int neighboursCount)
         {
             random = new Random();
             this.smoPassengers = smoPassengers;
             this.columns = columns;
             this.neighboursCount = neighboursCount;
-            idToNeighbours = smoPassengers.ToDictionary(x => x.AgentId, x => new List<string>());
+            idToNeighbours = smoPassengers.ToDictionary(x => x.Id, x => new List<string>());
         }
 
         public string[] GetNeighboursFor(string agentId, int agentPosition)
         {
             var freeNeighbours = idToNeighbours
-                .Where(pair => pair.Value.Count == neighboursCount)
+                .Where(pair => pair.Value.Count <= neighboursCount)
                 .ToList();
 
             var allCurrentNeighbours = idToNeighbours[agentId];
 
-            var leftNeighbourId = smoPassengers[agentPosition - 1].AgentId;
-            TryAddNeighbour(agentId, allCurrentNeighbours, leftNeighbourId);
+            if (agentPosition - 1 > 0)
+            {
+                var leftNeighbourId = smoPassengers[agentPosition - 1].Id;
+                TryAddNeighbour(agentId, allCurrentNeighbours, leftNeighbourId);
+            }
 
-            var rigthNeigbour = smoPassengers[agentPosition + 1].AgentId;
-            TryAddNeighbour(agentId, allCurrentNeighbours, rigthNeigbour);
+            if (agentPosition + 1 < smoPassengers.Length)
+            {
+                var rigthNeigbour = smoPassengers[agentPosition + 1].Id;
+                TryAddNeighbour(agentId, allCurrentNeighbours, rigthNeigbour);
+            }
 
-            var upNeighbour = smoPassengers[agentPosition - columns].AgentId;
-            TryAddNeighbour(agentId, allCurrentNeighbours, upNeighbour);
+            if (agentPosition - columns > 0)
+            {
+                var upNeighbour = smoPassengers[agentPosition - columns].Id;
+                TryAddNeighbour(agentId, allCurrentNeighbours, upNeighbour);
+            }
 
-            var downNeighbour = smoPassengers[agentPosition + columns].AgentId;
-            TryAddNeighbour(agentId, allCurrentNeighbours, downNeighbour);
+            if (agentPosition + columns < smoPassengers.Length)
+            {
+                var downNeighbour = smoPassengers[agentPosition + columns].Id;
+                TryAddNeighbour(agentId, allCurrentNeighbours, downNeighbour);
+            }
 
             freeNeighbours = freeNeighbours
-                .Where(pair => pair.Value.Count == neighboursCount)
+                .Where(pair => pair.Value.Count <= neighboursCount)
                 .Where(pair => !allCurrentNeighbours.Contains(pair.Key))
                 .ToList();
 
@@ -54,7 +66,7 @@ namespace TransportSystem.Api.Models
                 if (freeNeighbours.Count == 0)
                     continue;
                 var number = random.Next(0, freeNeighbours.Count -1);
-                var otherNeighbour = smoPassengers[number].AgentId;
+                var otherNeighbour = smoPassengers[number].Id;
                 randomNeighbours.Add(otherNeighbour);
                 idToNeighbours[otherNeighbour].Add(agentId);
                 freeNeighbours.RemoveAt(number);
