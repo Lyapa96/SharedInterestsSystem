ad8c8109a5b512480ec3e8d9561cb7d078c9c275
lms <lms@skbkontur.ru>
2019-05-29 11:06:27 +0500

    add Next to IterationResult

---
+++ TransportSystem.Api/src/Controllers/IterationResult.cs
@@ -0,0 +1,23 @@
+﻿using TransportSystem.Api.Models.TransportChooseAlgorithms;
+
+namespace TransportSystem.Api.Controllers
+{
+    public class IterationResult
+    {
+        public PassengerDto[] Passengers { get; set; }
+        public double AverageSatisfaction { get; set; }
+        public int IterationStep { get; set; }
+        public ChoiceTransportAlgorithmType AlgorithmType { get; set; }
+
+        public IterationResult Next(PassengerDto[] passengers, double averageSatisfaction)
+        {
+            return new IterationResult
+            {
+                IterationStep = IterationStep+1,
+                AlgorithmType = AlgorithmType,
+                AverageSatisfaction = averageSatisfaction,
+                Passengers = passengers,
+            };
+        }
+    }
+}
