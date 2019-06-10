5df5b5f129cec3fa1ea23cec59f7736197c90433
lms <lms@skbkontur.ru>
2019-06-10 16:13:16 +0500

    add quality chart

---
+++ TransportSystem.Web/ClientApp/src/components/SmoTransportQualityChart.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Highcharts from 'highcharts';
+import {
+    HighchartsChart, Chart, withHighcharts, XAxis, YAxis, Title, Subtitle, Legend, LineSeries
+} from 'react-jsx-highcharts';
+
+const plotOptions = {
+    series: {
+        pointStart: 0
+    }
+};
+
+const App = ({carQualityData, busQualityData}) => (
+    <div className="app">
+        <HighchartsChart plotOptions={plotOptions}>
+            <Chart />
+
+            <Title>Iterations results: average satisfaction passengers by transport</Title>
+
+            <Legend layout="vertical" align="right" verticalAlign="middle" />
+
+            <XAxis>
+                <XAxis.Title>Iterations</XAxis.Title>
+            </XAxis>
+
+            <YAxis>
+                <YAxis.Title>Average satisfaction passengers by transport</YAxis.Title>
+                <LineSeries name="Car" data={carQualityData} />
+                <LineSeries name="Bus" data={busQualityData} />
+            </YAxis>
+        </HighchartsChart>
+    </div>
+);
+
+export default withHighcharts(App, Highcharts);
