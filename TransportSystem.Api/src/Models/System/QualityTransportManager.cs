d9c444d23691d2ed96c396d99dd0be1e6ef851fe
liapustin <liapustin@yandex-team.ru>
2019-10-20 23:06:19 +0300

    update quality determination

--- TransportSystem.Api/src/Models/System/QualityTransportManager.cs
+++ TransportSystem.Api/src/Models/System/QualityTransportManager.cs
@@ -7,33 +7,52 @@ namespace TransportSystem.Api.Models.System
 {
     public static class QualityTransportManager
     {
-        public static void ChangeQuality(Passenger[][] passengers)
-        {
-            var carCount = passengers.Sum(x => x.Count(y => y.TransportType == TransportType.Car));
-            var rowCount = passengers.Length;
-            var columnCount = passengers.First().Length;
-            var passengersCount = rowCount*columnCount;
-
-            for (var i = 0; i < rowCount; i++)
-            for (var j = 0; j < columnCount; j++)
-            {
-                var passenger = passengers[i][j];
-                passenger.QualityCoefficient = passenger.TransportType == TransportType.Car
-                    ? Math.Round(GetQualityCoefficientForCar(carCount, passengersCount, passenger), 2)
-                    : Math.Round(GetQualityCoefficientForBus(passenger), 2);
-            }
-        }
+//        public static void ChangeQuality(Passenger[][] passengers)
+//        {
+//            var carCount = passengers.Sum(x => x.Count(y => y.TransportType == TransportType.Car));
+//            var rowCount = passengers.Length;
+//            var columnCount = passengers.First().Length;
+//            var passengersCount = rowCount*columnCount;
+//
+//            for (var i = 0; i < rowCount; i++)
+//            for (var j = 0; j < columnCount; j++)
+//            {
+//                var passenger = passengers[i][j];
+//                passenger.QualityCoefficient = passenger.TransportType == TransportType.Car
+//                    ? Math.Round(GetQualityCoefficientForCar(carCount, passengersCount, passenger), 2)
+//                    : Math.Round(GetQualityCoefficientForBus(passenger), 2);
+//            }
+//        }
 
         public static void ChangeQuality(Passenger[] passengers)
         {
             var carCount = passengers.Count(x => x.TransportType == TransportType.Car);
+            var bikeCount = passengers.Count(x => x.TransportType == TransportType.Bike);
             var passengersCount = passengers.Length;
 
             foreach (var passenger in passengers)
             {
-                passenger.QualityCoefficient = passenger.TransportType == TransportType.Car
-                    ? Math.Round(GetQualityCoefficientForCar(carCount, passengersCount, passenger), 2)
-                    : Math.Round(GetQualityCoefficientForBusInSmo(passenger), 2);
+                passenger.QualityCoefficient =
+                    GetQualityCoefficient(passenger.TransportType, carCount, bikeCount, passengersCount, passenger);
+            }
+        }
+
+        private static double GetQualityCoefficient(TransportType transportType, int carCount,int bikeCount, int passengersCount, Passenger passenger)
+        {
+            switch (transportType)
+            {
+                case TransportType.Car:
+                    return Math.Round(GetQualityCoefficientForCar(carCount, passengersCount, passenger), 2);
+                case TransportType.Bus:
+                    return Math.Round(GetQualityCoefficientForBusInSmo(passenger), 2);
+                case TransportType.Subway:
+                    return 0.6;
+                case TransportType.Bike:
+                   return Math.Round(GetQualityCoefficientForBike(bikeCount, passengersCount, passenger), 2);
+                case TransportType.Tram:
+                    return 0.4;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(transportType), transportType, null);
             }
         }
 
@@ -52,6 +71,17 @@ namespace TransportSystem.Api.Models.System
             if (passenger.Neighbors.Count(x => x.TransportType == TransportType.Car) < 2)
             {
                 var answer = 1 - (double) carCount/n;
+                return answer;
+            }
+
+            return 0.1;
+        }
+
+        private static double GetQualityCoefficientForBike(int bikeCount, int n, Passenger passenger)
+        {
+            if (passenger.Neighbors.Count(x => x.TransportType == TransportType.Bike) < 2)
+            {
+                var answer = 1 - (double)bikeCount / n - 0.25;
                 return answer;
             }
 
