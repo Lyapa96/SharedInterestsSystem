73b102a782163fda2d392bab6cc53f7cbc1be4ac
liapustin <liapustin@yandex-team.ru>
2019-11-10 13:52:18 +0300

    [back] add TransportInitData

---
+++ TransportSystem.Api/src/Models/Data/TransportInitData.cs
@@ -0,0 +1,10 @@
+﻿namespace TransportSystem.Api.Models.Data
+{
+    public class TransportInitData
+    {
+        public int PassengersCount { get; set; }
+        public int Columns { get; set; }
+        public int NeighboursCount { get; set; }
+        public TransportType[] AvailableTransportTypes { get; set; }
+    }
+}
